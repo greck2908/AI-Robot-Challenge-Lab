963cc625c0ece0192adeb0d17230b13ae22667d6
Ignite <robotics-lab-attendee@outlook.com>
2018-09-25 09:57:52 -0400

    aoki-kentaro@mki.co.jp: completed lab

--- src/chatbot/talk-to-my-robot.py
+++ src/chatbot/talk-to-my-robot.py
@@ -2,6 +2,7 @@ import json
 import requests
 import re
 from io import BytesIO
+# import os.system
 
 # Used to call Python 2.7 from 3.6
 import subprocess
@@ -12,9 +13,9 @@ from botbuilder.schema import (Activity, ActivityTypes)
 
 
 # Settings
-LUIS_APP_ID = 'UPDATE_THIS_KEY'
-LUIS_SUBSCRIPTION_KEY = 'UPDATE_THIS_KEY'
-COMPUTER_VISION_SUBSCRIPTION_KEY = "UPDATE_THIS_KEY"
+LUIS_APP_ID = '357b6036-c961-4bb0-9aa9-bbbcdf1e32f0'
+LUIS_SUBSCRIPTION_KEY = 'b7f33ef452a544079fb86356479f9300'
+COMPUTER_VISION_SUBSCRIPTION_KEY = "f35e56ef483640fc9153d69c5c123266"
 
 COMPUTER_VISION_ANALYZE_URL = "https://westus.api.cognitive.microsoft.com/vision/v2.0/analyze"
 BOT_APP_ID = ''
@@ -28,26 +29,26 @@ class ComputerVisionApiService:
     @staticmethod
     def analyze_image(image_url):
         #Analyze Image Request Headers and Parameters (do not uncomment this line)
-        # headers = {
-        #     'Ocp-Apim-Subscription-Key': COMPUTER_VISION_SUBSCRIPTION_KEY,
-        #     'Content-Type': 'application/octet-stream'
-        # }
-        # params = {'visualFeatures': 'Color'}
+        headers = {
+            'Ocp-Apim-Subscription-Key': COMPUTER_VISION_SUBSCRIPTION_KEY,
+            'Content-Type': 'application/octet-stream'
+        }
+        params = {'visualFeatures': 'Color'}
 
         #Get Image Bytes Content (do not uncomment this line)
-        #image_data = BytesIO(requests.get(image_url).content)
+        image_data = BytesIO(requests.get(image_url).content)
         
         try:
             #Process Image (do not uncomment this line)
-            # print('Processing image: {}'.format(image_url))
-            # response = requests.post(COMPUTER_VISION_ANALYZE_URL, headers=headers, params=params, data=image_data)
-            # response.raise_for_status()
-            # analysis = response.json()
-            # dominant_color = analysis["color"]["dominantColors"][0]
+            print('Processing image: {}'.format(image_url))
+            response = requests.post(COMPUTER_VISION_ANALYZE_URL, headers=headers, params=params, data=image_data)
+            response.raise_for_status()
+            analysis = response.json()
+            dominant_color = analysis["color"]["dominantColors"][0]
             
-            # return dominant_color
+            return dominant_color
 
-            return None #REMOVE this line when you uncomment the line above
+            # return None #REMOVE this line when you uncomment the line above
         except Exception as e:
             print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
@@ -61,28 +62,28 @@ class LuisApiService:
     @staticmethod
     def post_utterance(message):
         #Post Utterance Request Headers and Params (do not uncomment this line)
-        # headers = {'Ocp-Apim-Subscription-Key': LUIS_SUBSCRIPTION_KEY}
-        # params = {
-        #     # Query parameter
-        #     'q': message,
-        #     # Optional request parameters, set to default values
-        #     'timezoneOffset': '0',
-        #     'verbose': 'false',
-        #     'spellCheck': 'false',
-        #     'staging': 'false',
-        # }
+        headers = {'Ocp-Apim-Subscription-Key': LUIS_SUBSCRIPTION_KEY}
+        params = {
+            # Query parameter
+            'q': message,
+            # Optional request parameters, set to default values
+            'timezoneOffset': '0',
+            'verbose': 'false',
+            'spellCheck': 'false',
+            'staging': 'false',
+        }
 
         try:
             #LUIS Response (do not uncomment this line)
-            # r = requests.get('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/%s' % LUIS_APP_ID, headers=headers, params=params)
-            # topScoreIntent = r.json()['topScoringIntent']
-            # entities = r.json()['entities']
-            # intent = topScoreIntent['intent'] if topScoreIntent['score'] > 0.5 else 'None' 
-            # entity = entities[0] if len(entities) > 0 else None
+            r = requests.get('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/%s' % LUIS_APP_ID, headers=headers, params=params)
+            topScoreIntent = r.json()['topScoringIntent']
+            entities = r.json()['entities']
+            intent = topScoreIntent['intent'] if topScoreIntent['score'] > 0.5 else 'None' 
+            entity = entities[0] if len(entities) > 0 else None
             
-            # return LuisResponse(intent, entity['entity'], entity['type']) if entity else LuisResponse(intent)
+            return LuisResponse(intent, entity['entity'], entity['type']) if entity else LuisResponse(intent)
             
-            return None #REMOVE this line when you uncomment the line above
+            # return None #REMOVE this line when you uncomment the line above
 
         except Exception as e:
             print("[Errno {0}] {1}".format(e.errno, e.strerror))
@@ -103,7 +104,7 @@ class BotRequestHandler:
         activity = context.activity
         if activity.text:
             #Get LUIS result (do not uncomment this line)
-            #luis_result = LuisApiService.post_utterance(activity.text)
+            luis_result = LuisApiService.post_utterance(activity.text)
 
             response = await BotRequestHandler.create_reply_activity(activity, f'Top Intent: {luis_result.intent}.')
             await context.send_activity(response)
@@ -114,7 +115,7 @@ class BotRequestHandler:
                 print("MoveArm") 
             elif luis_result.intent == 'MoveGrippers':
                 # Set Move Grippers Handler (do not uncomment this line)
-                # BotCommandHandler.move_grippers(luis_result.entity_value)
+                BotCommandHandler.move_grippers(luis_result.entity_value)
 
                 print("MoveGrippers")
             elif luis_result.intent == 'ShowStats':
@@ -143,16 +144,16 @@ class BotRequestHandler:
     
     async def process_image(activity: Activity, context: TurnContext):
         # Implement Process Image Method (do not uncomment this line)
-        # image_url = BotRequestHandler.get_image_url(activity.attachments)
+        image_url = BotRequestHandler.get_image_url(activity.attachments)
 
-        # if image_url:
-        #     dominant_color = ComputerVisionApiService.analyze_image(image_url)
-        #     response = await BotRequestHandler.create_reply_activity(activity, f'Do you need a {dominant_color} cube? Let me find one for you!')
-        #     await context.send_activity(response)
-        #     BotCommandHandler.move_cube(dominant_color)
-        # else:
-        #     response = await BotRequestHandler.create_reply_activity(activity, 'Please provide a valid instruction or image.')
-        #     await context.send_activity(response)
+        if image_url:
+            dominant_color = ComputerVisionApiService.analyze_image(image_url)
+            response = await BotRequestHandler.create_reply_activity(activity, f'Do you need a {dominant_color} cube? Let me find one for you!')
+            await context.send_activity(response)
+            BotCommandHandler.move_cube(dominant_color)
+        else:
+            response = await BotRequestHandler.create_reply_activity(activity, 'Please provide a valid instruction or image.')
+            await context.send_activity(response)
 
         return None 
 
@@ -198,16 +199,16 @@ class BotCommandHandler:
 
     def move_grippers(action):
         #Implement Move Grippers Command (do not uncomment this line)
-        # print(f'{action} grippers... wait a few seconds')
-        # # launch your python2 script using bash
-        # python2_command = "python2.7 bot-move-grippers.py -a {}".format(action)  
+        print(f'{action} grippers... wait a few seconds')
+        # launch your python2 script using bash
+        python2_command = "python2.7 bot-move-grippers.py -a {}".format(action)  
 
-        # process = subprocess.Popen(python2_command.split(), stdout=subprocess.PIPE)
-        # output, error = process.communicate()  # receive output from the python2 script
+        process = subprocess.Popen(python2_command.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()  # receive output from the python2 script
       
-        # print('done moving grippers . . .')
-        # print('returncode: '  + str(process.returncode))
-        # print('output: ' + output.decode("utf-8"))
+        print('done moving grippers . . .')
+        print('returncode: '  + str(process.returncode))
+        print('output: ' + output.decode("utf-8"))
         
         return None
 
